fdc976242623cc193f51de7163876ce35e6d197c
http-Figueiredo <http.figueiredo@gmail.com>
2025-11-10 00:57:12 -0400

    Adicionado a função 'CREATE' do CRUD

--- script.js
+++ script.js
@@ -3,12 +3,17 @@
 // 2 - Sincronizar Conteúdo das variáveis com o HTML
 // 3 - Atualiza as variáveis
 
-console.log("Oi");
-
+// Código de front end: Web ->
 const $meuForm = document.querySelector('form');
 console.log($meuForm);
 
-$meuForm.addEventListener('submit', function criaPostController(infosDoEvento) {
-    infosDoEvento.preventDefault();
-    console.log("Post criado")
+//CRUD: CREATE
+$meuForm.addEventListener('submit', function criaPostController(info) { //Detecta o primeiro evento 'submit' e aciona a função. 
+    info.preventDefault(); //Previne que a página recarregue quando enviar o formulário
+    console.log("Post criado");
+    const $campoCriaPost = document.querySelector('input[name="campoCriaPost"]'); //'input[name=""]' para detectar um input específico 
+    const $listaDePosts = document.querySelector('.listaDePosts');
+    
+    $listaDePosts.insertAdjacentHTML('beforeend', `<li>${$campoCriaPost.value}</li>`);
+    $campoCriaPost.value = '';
 });
