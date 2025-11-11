0e5b4bee7e6f4336a254b81d26f65b618ae8a88a
http-Figueiredo <http.figueiredo@gmail.com>
2025-11-11 18:48:25 -0400

    feat: adiciona a função READ

--- script.js
+++ script.js
@@ -3,17 +3,51 @@
 // 2 - Sincronizar Conteúdo das variáveis com o HTML
 // 3 - Atualiza as variáveis
 
+// Banco de Dados -->
+
+const miniRedeSocial = {
+    usuarios: [
+        {
+            username: 'http-figueiredo',
+        }
+    ],
+    posts: [
+        {
+            id: 1,
+            owner: 'http-figueiredo',
+            content: 'Meu primeiro tweet'
+        }
+    ],
+    criaPost(dados, htmlOnly = false) { // htmlOnly necessario, se não o post com id: 1, aparece duplicado (pois cria na memória e no html)
+        if (!htmlOnly){
+            miniRedeSocial.posts.push({
+                id: miniRedeSocial.posts.length + 1,
+                owner: dados.owner,
+                content: dados.content, 
+            });
+        }
+        //Cria Post no HTML
+        const $listaDePosts = document.querySelector('.listaDePosts');
+        $listaDePosts.insertAdjacentHTML('afterbegin', `<li>${dados.content}</li>`);
+    }
+};
+
 // Código de front end: Web ->
-const $meuForm = document.querySelector('form');
-console.log($meuForm);
+    const $meuForm = document.querySelector('form');
+    console.log($meuForm);
 
 //CRUD: CREATE
 $meuForm.addEventListener('submit', function criaPostController(info) { //Detecta o primeiro evento 'submit' e aciona a função. 
     info.preventDefault(); //Previne que a página recarregue quando enviar o formulário
     console.log("Post criado");
-    const $campoCriaPost = document.querySelector('input[name="campoCriaPost"]'); //'input[name=""]' para detectar um input específico 
-    const $listaDePosts = document.querySelector('.listaDePosts');
+    const $campoCriaPost = document.querySelector('input[name="campoCriaPost"]'); //'input[name=""]' para detectar um input específico dentro do formulário
     
-    $listaDePosts.insertAdjacentHTML('beforeend', `<li>${$campoCriaPost.value}</li>`);
+    miniRedeSocial.criaPost({ owner: "http-figueiredo", content: $campoCriaPost.value });
+
     $campoCriaPost.value = '';
 });
+
+//CRUD: READ
+miniRedeSocial.posts.forEach(({ owner, content }) => {
+    miniRedeSocial.criaPost({ owner: owner, content: content }, true); // esse true serve para identificar que será criado na memória
+})
