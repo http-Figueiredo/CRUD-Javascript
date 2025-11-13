727a4861ff35365eb2f518c0f41c407df82be98f
http-Figueiredo <http.figueiredo@gmail.com>
2025-11-12 20:52:38 -0400

    refactor: Transforma a maneira de LER (READ) antiga em uma função para facilitar seu uso posteriormente

--- script.js
+++ script.js
@@ -17,8 +17,13 @@ const miniRedeSocial = {
             owner: 'http-figueiredo',
             content: 'Meu primeiro tweet'
         }
-    ],
-    criaPost(dados, htmlOnly = false) { // htmlOnly necessario, se não o post com id: 1, aparece duplicado (pois cria na memória e no html)
+    ], 
+    readPosts() {
+        miniRedeSocial.posts.forEach(({ owner, content }) => {
+            miniRedeSocial.createPost({ owner: owner, content: content }, true); // esse true serve para identificar que será criado na memória
+        })
+    },
+    createPost(dados, htmlOnly = false) { // htmlOnly necessario, se não o post com id: 1, aparece duplicado (pois cria na memória e no html)
         if (!htmlOnly){
             miniRedeSocial.posts.push({
                 id: miniRedeSocial.posts.length + 1,
@@ -37,17 +42,15 @@ const miniRedeSocial = {
     console.log($meuForm);
 
 //CRUD: CREATE
-$meuForm.addEventListener('submit', function criaPostController(info) { //Detecta o primeiro evento 'submit' e aciona a função. 
+$meuForm.addEventListener('submit', function createPostController(info) { //Detecta o primeiro evento 'submit' e aciona a função. 
     info.preventDefault(); //Previne que a página recarregue quando enviar o formulário
     console.log("Post criado");
     const $campoCriaPost = document.querySelector('input[name="campoCriaPost"]'); //'input[name=""]' para detectar um input específico dentro do formulário
     
-    miniRedeSocial.criaPost({ owner: "http-figueiredo", content: $campoCriaPost.value });
+    miniRedeSocial.createPost({ owner: "http-figueiredo", content: $campoCriaPost.value });
 
     $campoCriaPost.value = '';
 });
 
 //CRUD: READ
-miniRedeSocial.posts.forEach(({ owner, content }) => {
-    miniRedeSocial.criaPost({ owner: owner, content: content }, true); // esse true serve para identificar que será criado na memória
-})
+miniRedeSocial.readPosts();
